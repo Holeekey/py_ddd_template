872df873417ca9312aed5568e127644f21f9e8e6
d.brunoD4T <114121356+Holeekey@users.noreply.github.com>
2024-11-16 17:34:15 -0400

    build: :construction_worker: dockerfile and requirements versions updated

--- src/main.py
+++ src/main.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
 
    reload = config.STAGE == "dev"
 
-   uvicorn.run("main:app", host="127.0.0.1", port=config.APP_PORT, reload=reload)
+   uvicorn.run("main:app", host="0.0.0.0", port=config.APP_PORT, reload=reload)
